aa3212bffe6391ae46f1c0914b106b46ca23e24c
Alex Howlett <alex.howlett@live.co.uk>
2024-09-04 18:34:56 +0100

    start basic layout for user-login page: image added

--- ltc/src/app/user-login/page.tsx
+++ ltc/src/app/user-login/page.tsx
@@ -1,9 +1,19 @@
-//add metadata to this page
+/*
+- add metadata 
+- add image for user log in (free img)
+- make image fit screen
+- create a form for user login 
+*/
+import Image from "next/image";
+import userLoginPic from "../../../public/pexels-sheinshine-3127880.jpg";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>User login</h1>
+    <main className="flex max-h-screen flex-col items-center justify-between">
+      <Image
+        src={userLoginPic}
+        alt="photo of a message saying JOIN US spelled out in scrabble letters"
+      />
     </main>
   );
 }
