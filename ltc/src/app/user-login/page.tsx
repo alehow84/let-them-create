e000f7cbe58f86e94f5b163b75fbd1f1970e034d
Alex Howlett <alex.howlett@live.co.uk>
2024-09-05 18:10:55 +0100

    amend global styling and outline form for user-login page

--- ltc/src/app/user-login/page.tsx
+++ ltc/src/app/user-login/page.tsx
@@ -9,11 +9,35 @@ import userLoginPic from "../../../public/pexels-sheinshine-3127880.jpg";
 
 export default function Page() {
   return (
-    <main className="flex max-h-screen flex-col items-center justify-between">
-      <Image
-        src={userLoginPic}
-        alt="photo of a message saying JOIN US spelled out in scrabble letters"
-      />
-    </main>
+    <div className="max-h-screen flex mx-auto">
+      <div className="max-w-[650px]">
+        <Image
+          src={userLoginPic}
+          alt="photo of a message saying JOIN US spelled out in scrabble letters"
+        />
+      </div>
+
+      <h1 className="text-center">Log in</h1>
+      <form className="flex flex-col justify-center mx-auto space-y-8">
+        <input
+          type="text"
+          id="email-address"
+          placeholder="Email address"
+          className="py-4 px-10 max-w-sm mx-auto bg-white rounded-xl shadow-md shadow-black"
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="Password"
+          className="py-4 px-10 max-w-sm mx-auto bg-white rounded-xl shadow-md shadow-black"
+        />
+        <button
+          type="submit"
+          className="py-4 px-10 max-w-sm mx-auto bg-orange-600 shadow-md rounded-xl text-white hover:bg-orange-400  hover:text-blue-600"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
