d918aaaefb860dc1e5f1ef3c74600cead1cfba34
Alex Howlett <alex.howlett@live.co.uk>
2024-09-07 20:03:45 +0100

    Finish UI for Footer component

--- ltc/src/app/components/Footer.tsx
+++ ltc/src/app/components/Footer.tsx
@@ -2,28 +2,36 @@ import Link from "next/link";
 import Image from "next/image";
 import Insta from "../../../public/icons/instagram.svg";
 import TikTok from "../../../public/icons/tiktok.svg";
+import Copyright from "../../../public/icons/copyright.svg";
 
 export default function Footer() {
   return (
     <footer className="flex items-center w-screen bg-slate-light p-6">
-      <Link
-        href="https://www.instagram.com/"
-        className="scale-100 hover:scale-125 mr-8"
-      >
-        <Image src={Insta} height={40} width={40} alt="instagram" />
-      </Link>
-      <Link
-        href="https://www.tiktok.com/en/"
-        className="scale-100 hover:scale-125 mr-4"
-      >
-        <Image src={TikTok} height={40} width={40} alt="Tik Tok" />
-      </Link>
-      <Link
-        href="/staff-login"
-        className="text-white rounded-full hover:scale-125 ml-auto"
-      >
-        Staff Login
-      </Link>
+      <div className="flex">
+        <Link
+          href="https://www.instagram.com/"
+          className="scale-100 hover:scale-125 mr-8"
+        >
+          <Image src={Insta} height={40} width={40} alt="instagram" />
+        </Link>
+        <Link
+          href="https://www.tiktok.com/en/"
+          className="scale-100 hover:scale-125 mr-4"
+        >
+          <Image src={TikTok} height={40} width={40} alt="Tik Tok" />
+        </Link>
+      </div>
+      <div className="flex text-white mx-auto">
+        <Image src={Copyright} height={20} width={20} alt="copyright" />
+        <strong className="hover:cursor-default">2024 Let Them Create</strong>
+      </div>
+      <div>
+        <Link href="/staff-login">
+          <p className="text-white rounded-full hover:scale-125 transition-transform">
+            Staff Login
+          </p>
+        </Link>
+      </div>
     </footer>
   );
 }
--- ltc/src/app/page.tsx
+++ ltc/src/app/page.tsx
@@ -2,14 +2,12 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
 export default function Home() {
   return (
-    <>
-      <body className="h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <h1>Let Them Create Homepage</h1>
-        </main>
-        <Footer />
-      </body>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <h1>Let Them Create Homepage</h1>
+      </main>
+      <Footer />
+    </div>
   );
 }
