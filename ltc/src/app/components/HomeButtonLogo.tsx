0fa65014e141ede6061a58df995cfa2cfd41d69a
Alex Howlett <alex.howlett@live.co.uk>
2024-09-08 16:44:55 +0100

    fix scale bug on HomeButtonLogo

--- ltc/src/app/components/HomeButtonLogo.tsx
+++ ltc/src/app/components/HomeButtonLogo.tsx
@@ -4,8 +4,10 @@ import Logo from "../../../public/icons/HomeButtonLogo.svg";
 
 export default function HomeButtonLogo({ size }: { size: number }) {
   return (
-    <Link href="/" className="hover:scale-110">
-      <Image src={Logo} alt="Home" width={size} height={size} />
+    <Link href="/">
+      <div className="hover:scale-110">
+        <Image src={Logo} alt="Home" width={size} height={size} />
+      </div>
     </Link>
   );
 }
