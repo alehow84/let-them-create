29cf52998071e8e4cfeccc7a2b9c27e75534d531
Alex Howlett <alex.howlett@live.co.uk>
2024-09-08 19:51:03 +0100

    add transition to HomeButtonLogo & submit buttons

--- ltc/src/app/components/HomeButtonLogo.tsx
+++ ltc/src/app/components/HomeButtonLogo.tsx
@@ -5,7 +5,7 @@ import Logo from "../../../public/icons/HomeButtonLogo.svg";
 export default function HomeButtonLogo({ size }: { size: number }) {
   return (
     <Link href="/">
-      <div className="hover:scale-110">
+      <div className="hover:scale-110 transition ease-in-out duration-200">
         <Image src={Logo} alt="Home" width={size} height={size} />
       </div>
     </Link>
--- ltc/src/app/staff-login/page.tsx
+++ ltc/src/app/staff-login/page.tsx
@@ -34,7 +34,7 @@ export default function Page() {
             />
             <button
               type="submit"
-              className="py-4 px-10 max-w-sm mx-auto text-white bg-orange shadow-md rounded-xl hover:bg-amber  hover:text-blue"
+              className="py-4 px-10 max-w-sm mx-auto text-white bg-orange shadow-md rounded-xl hover:bg-amber  hover:text-blue transition ease-in-out duration-200"
             >
               Submit
             </button>
