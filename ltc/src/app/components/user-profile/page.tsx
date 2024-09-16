e1f4ef8f3b26b010c6d0cbd5d078ea105478a405
Alex Howlett <alex.howlett@live.co.uk>
2024-09-16 11:00:38 +0100

    add skeleton of code required to render user-profile

--- ltc/src/app/components/user-profile/page.tsx
+++ ltc/src/app/components/user-profile/page.tsx
@@ -6,7 +6,23 @@ import ProtectedRoute from "../ProtectedRoute";
 import { useAuth } from "../../contexts/AuthContext";
 import { useRouter } from "next/router";
 
-export default function Page() {
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+
+// export async function getServerSideProps(context) {
+//   const { userId } = context.params;
+
+//   // Fetch user data from Firebase - make sure to replace fetchUserDataFromFirebase(userId) with actual data fetching logic.
+//   const userData = await fetchUserDataFromFirebase(userId);
+
+//   return {
+//     props: {
+//       userData,
+//     },
+//   };
+// }
+
+//make sure to add {userDara} as a param in UserProfile, then you can render by accessing certain info off the userData object.
+export default function UserProfile() {
   const { user, logOut } = useAuth();
   const router = useRouter();
   return (
